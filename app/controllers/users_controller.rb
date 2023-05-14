e2ccbe4fa1a8d5ae0a39d0c12549dbf061c4c6ee
takeuchisaki <129586704+takeuchisaki@users.noreply.github.com>
2023-05-14 15:20:07 +0000

    エラー修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def index
     @users = User.all
     @book = Book.new
@@ -7,7 +9,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @book = Book.new
-    @books = Book.all
+    @books = @user.books
   end
 
   def edit
@@ -15,12 +17,11 @@ class UsersController < ApplicationController
   end
 
   def update
-    user = User.find(params[:id])
-    if user.update(user_params)
+    @user = User.find(params[:id])
+    if @user.update(user_params)
       flash[:notice] = "You have updated user successfully."
-      redirect_to user_path(user)
+      redirect_to user_path(@user)
     else
-      @user = User.find(params[:id])
       render :edit
     end
   end
@@ -28,7 +29,16 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :b)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    if unless user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
+
+    end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_one_attached :profile_image
-  
+
   has_many :books, dependent: :destroy
-  
+
   validates :name, length: { in: 2..20 }, uniqueness: true
-  validates :body, length: { maximum:50 } 
-  
-  
+  validates :introduction, length: { maximum:50 }
+
+
   def get_profile_image(width,height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
@@ -19,5 +19,5 @@ class User < ApplicationRecord
     end
       profile_image.variant(resize_to_limit: [width, height]).processed
   end
-  
+
 end
