d6685347647a07372dbf01a2f54863cf16292192
takeuchisaki <129586704+takeuchisaki@users.noreply.github.com>
2023-05-24 00:50:18 +0000

    finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,18 @@
 class BooksController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update, :destroy]
+  
   def index
     @book = Book.new
     @books = Book.all
   end
 
   def create
-    book = Book.new(book_params)
-    book.user_id = current_user.id
-    if book.save
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
       flash[:notice] = "You have created book successfully."
-      redirect_to book_path(book)
+      redirect_to book_path(@book)
     else
-      @book = Book.new
       @books = Book.all
       render :index
     end
@@ -28,12 +29,11 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = "You have updated book successfully."
-      redirect_to book_path(book)
+      redirect_to book_path(@book)
     else
-      @book = Book.find(params[:id])
       render :edit
     end
   end
@@ -48,5 +48,12 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+  
+  def is_matching_login_user
+    book = Book.find(params[:id])
+    unless book.user == current_user
+      redirect_to books_path
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,14 +31,12 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
-
+    
   def is_matching_login_user
     user = User.find(params[:id])
-    if unless user == current_user
+    unless user == current_user
       redirect_to user_path(current_user)
     end
   end
-
-    end
 
 end
