bda813278113094621a61a365ba0ad285fb1a3b2
takeuchisaki <129586704+takeuchisaki@users.noreply.github.com>
2023-07-28 04:35:38 +0000

    フォロー機能/非同期通信

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,15 +1,17 @@
 class RelationshipsController < ApplicationController
-  
+
   def create
     current_user.follow(params[:user_id])
-    redirect_to request.referer
+    @user = User.find(params[:user_id])
+    # redirect_to request.referer
   end
-  
+
   def destroy
     current_user.unfollow(params[:user_id])
-    redirect_to request.referer
+    @user = User.find(params[:user_id])
+    # redirect_to request.referer
   end
-  
+
   def followings
     user = User.find(params[:user_id])
     @users = user.followings
